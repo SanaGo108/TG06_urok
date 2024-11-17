b911305c15671e063bd2218aa2006bc331e5f589
SanaGo108 <oksana135@mail.ru>
2024-11-17 15:16:06 +0300

    add

---
+++ bot.py
@@ -0,0 +1,67 @@
+import asyncio
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from aiogram import Bot, Dispatcher, F
+from aiogram. filters import CommandStart, Command
+from aiogram. types import Message, FSInputFile
+from aiogram. fsm. context import FSMContext
+from aiogram. fsm.state import State, StatesGroup
+from aiogram. fsm. storage. memory import MemoryStorage
+
+from config import TOKEN
+import sqlite3
+import aiohttp
+import logging
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+logging.basicConfig(level=logging.INFO)
+
+button_registr = KeyboardButton(text="Регистрация в телеграм-боте")
+button_exchange_rates = KeyboardButton(text="Курс валют")
+button_tips = KeyboardButton(text="Советы по экономии")
+button_finances = KeyboardButton(text="Личные финансы")
+
+keyboard = ReplyKeyboardMarkup(keyboard=[
+    [button_registr, button_exchange_rates],
+    [button_tips, button_finances]
+    ], resize_keyboard=True)
+
+conn = sqlite3.connect('user.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    telegram_id INTEGER UNIQUE,
+    name TEXT,
+    category1 TEXT,
+    category2 TEXT,
+    category3 TEXT,
+    expenses1 REAL,
+    expenses2 REAL,
+    expenses3 REAL
+    )
+''')
+
+conn.commit()
+
+class FinancesForm(StatesGroup):
+    category1 = State()
+    expenses1 = State()
+    category2 = State()
+    expenses2 = State()
+    category3 = State()
+    expenses3 = State()
+
+@dp.message(Command('start'))
+async def send_start(message: Message):
+   await message.answer("Привет! Я ваш личный финансовый помощник. Выберите одну из опций в меню:", reply_markup=keyboards)
+   
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
